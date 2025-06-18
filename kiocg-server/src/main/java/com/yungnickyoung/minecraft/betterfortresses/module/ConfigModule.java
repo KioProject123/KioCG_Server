b655cd068f175446b37a8003f818abd80494f279
KioProject <1105919949@qq.com>
2025-06-18 13:59:17 +0800

    goodbye YUNG

--- kiocg-server/src/main/java/com/yungnickyoung/minecraft/betterfortresses/module/ConfigModule.java
+++
@@ -1,9 +0,0 @@
-package com.yungnickyoung.minecraft.betterfortresses.module;
-
-public class ConfigModule {
-    public General general = new General();
-
-    public static class General {
-        public boolean disableVanillaFortresses = true;
-    }
-}
--- kiocg-server/src/main/java/com/yungnickyoung/minecraft/betterwitchhuts/module/ConfigModule.java
+++
@@ -1,9 +0,0 @@
-package com.yungnickyoung.minecraft.betterwitchhuts.module;
-
-public class ConfigModule {
-    public General general = new General();
-
-    public static class General {
-        public boolean disableVanillaWitchHuts = true;
-    }
-}
